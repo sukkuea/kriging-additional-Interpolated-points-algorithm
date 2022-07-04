1b41a89a9c1f6d1bb057b4a44c393e7f9b8c75ad
Kittinut Pramhan <tong.kittinut@riseaccel.com>
2022-07-05 03:36:26 +0700

    Updates

---
+++ precache-manifest.87d5b3fa262044cf9f4d4c9aef79982e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5888289b01cc9ad27338",
+    "url": "/kriging-additional-interpolated-points-algorithm-2/static/css/main.4c0d5fe7.chunk.css"
+  },
+  {
+    "revision": "5888289b01cc9ad27338",
+    "url": "/kriging-additional-interpolated-points-algorithm-2/static/js/main.0b4dac93.chunk.js"
+  },
+  {
+    "revision": "7cdbda5ea0cf1fc179d7",
+    "url": "/kriging-additional-interpolated-points-algorithm-2/static/js/runtime~main.39e2d711.js"
+  },
+  {
+    "revision": "c6bbbedb8aafc2d0f198",
+    "url": "/kriging-additional-interpolated-points-algorithm-2/static/css/2.048f6f41.chunk.css"
+  },
+  {
+    "revision": "c6bbbedb8aafc2d0f198",
+    "url": "/kriging-additional-interpolated-points-algorithm-2/static/js/2.de44ff2b.chunk.js"
+  },
+  {
+    "revision": "392470c18f1bca56e1aaba9a3af0645e",
+    "url": "/kriging-additional-interpolated-points-algorithm-2/index.html"
+  }
+];
